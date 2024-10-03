dcb280e47cc4a1274d567207418da53ae3d87222
Md. Saminul Amin <saminul.amin@gmail.com>
2024-10-03 17:54:18 +0600

    dummy progress bar added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
+
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -29,6 +32,12 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          timeout={10000}
+          onTimeout={() => {
+            handleSelectAnswer(null);
+          }}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
